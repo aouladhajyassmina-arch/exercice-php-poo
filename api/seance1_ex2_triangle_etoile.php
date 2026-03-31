db68df3fadd84e6db3469e9cd18791bdb92465eb
aouladhajyassmina <aouladhajyasmina@gmail.com>
2026-03-31 17:42:19 +0100

    first commit

---
+++ api/seance1_ex2_triangle_etoile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+fonction triangle($a){
+for($i=1;$i<=$a;$i++){
+for($j=$i;$j<=$a;$j++){
+      echo("&nbsp;&nbsp");
+}
+for($p=1;$p<=(2*$i-1);$p++){
+    echo"*";
+}
+echo"<br>";
+    
+}
+  
+}
+triangle(6)
+
+
+?>
+    
+</body>
+</html>
