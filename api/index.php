beba3a782cfdedd29d2dbf18b8a41bd03ba495e3
aouladhajyassmina-arch <aouladhajyassmina@gmail.com>
2026-04-10 09:47:11 +0100

    yet

--- api/index.php
+++ api/index.php
@@ -373,7 +373,6 @@ li {
     <div class="buttons">
         <button onclick="showAtelier(1)">Atelier 1</button>
         <button onclick="showAtelier(2)">Atelier 2</button>
-        <button onclick="showAtelier(3)">Atelier 3</button>
     </div>
 
     <div class="grid">
@@ -427,30 +426,6 @@ li {
             <a href="exercice8.php">Voir exercice 8</a>
             <a href="r8.pdf">Voir rapport</a>
         </div>
-         <!-- ATELIER 3 -->
-        <div class="card atelier3">
-            <h3>Séance 1</h3>
-            <a href="seance1_ex2_etoile.php">Voir exercice 9</a>
-            <a href="r1.pdf">Voir rapport</a>
-        </div>
-
-        <div class="card atelier1">
-            <h3>Séance 2</h3>
-            <a href="seance1_ex1.php">Voir exercice 10</a>
-            <a href="r2.pdf">Voir rapport</a>
-        </div>
-
-        <div class="card atelier1">
-            <h3>Séance 3</h3>
-            <a href="exercice3.php">Voir exercice 11</a>
-            <a href="r3.pdf">Voir rapport</a>
-        </div>
-
-        <div class="card atelier1">
-            <h3>Séance 4</h3>
-            <a href="exercice4.php">Voir exercice 12</a>
-            <a href="r4.pdf">Voir rapport</a>
-        </div>
 
     </div>
 </section>
@@ -495,16 +470,13 @@ function sendMessage(e){
 function showAtelier(num) {
     const atelier1 = document.querySelectorAll('.atelier1');
     const atelier2 = document.querySelectorAll('.atelier2');
-    const atelier3 = document.querySelectorAll('.atelier3');
 
     if (num === 1) {
         atelier1.forEach(el => el.classList.remove('hidden'));
         atelier2.forEach(el => el.classList.add('hidden'));
-        atelier3.forEach(el => el.classList.remove('hidden'));
     } else {
         atelier1.forEach(el => el.classList.add('hidden'));
         atelier2.forEach(el => el.classList.remove('hidden'));
-        atelier3.forEach(el => el.classList.add('hidden'));
     }
 }
 </script>
